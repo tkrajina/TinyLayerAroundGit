4bd15c592fe109fcdb7a874de2252f7a17b52a26
tkrajina <tkrajina@gmail.com>
2011-06-06 22:03:34 +0200

    git custom command

--- src/tinylayeraroundgit/actions/RunGitCommandAction.java
+++ src/tinylayeraroundgit/actions/RunGitCommandAction.java
@@ -1,14 +1,49 @@
 package tinylayeraroundgit.actions;
 
 import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.dialogs.IInputValidator;
+import org.eclipse.jface.dialogs.InputDialog;
+
+import tinylayeraroundgit.git.GitCommand;
 
 
 public class RunGitCommandAction extends AbstractActionDelegate {
+	
+	class GitCommandInputValidator implements IInputValidator {
+
+		@Override
+		public String isValid( String newText ) {
+			if( newText == null || newText.trim().length() == 0 ) {
+				return "Invalid git command";
+			}
+			
+			if( ! newText.startsWith( "git " ) ) {
+				return "Your command must start with \"git \"";
+			}
+			
+			return null;
+		}
+		
+	}
 
 	@Override
 	public void run( IAction action ) {
-		// TODO Auto-generated method stub
-
+		InputDialog dialog = new InputDialog( getShell(), "Git command", "Git command", "git ", new GitCommandInputValidator() );
+		
+		dialog.create();
+		dialog.open();
+		
+		String command = dialog.getValue();
+		
+		if( command != null ) {
+			GitCommand gitCommand = new GitCommand( command, true );
+			
+			try {
+				gitCommand.executeOn( getSelectedResources() );
+			} catch ( InterruptedException e ) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
