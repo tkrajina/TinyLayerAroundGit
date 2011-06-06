b1b0bb9c03d921a5759b768e6e2807b2cccd5b7a
tkrajina <tkrajina@gmail.com>
2011-06-06 21:34:50 +0200

    Merge branches

--- src/tinylayeraroundgit/actions/MergeBranchAction.java
+++ src/tinylayeraroundgit/actions/MergeBranchAction.java
@@ -1,14 +1,19 @@
 package tinylayeraroundgit.actions;
 
 import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.wizard.WizardDialog;
+
+import tinylayeraroundgit.wizard.MergeBranchWizard;
 
 
 public class MergeBranchAction extends AbstractActionDelegate {
 
 	@Override
 	public void run( IAction action ) {
-		// TODO Auto-generated method stub
-
+		MergeBranchWizard wizard = new MergeBranchWizard( getSelectedResources() );
+		WizardDialog dialog = new WizardDialog( getShell(), wizard );
+		dialog.create();
+		dialog.open();
 	}
 
 }
---
+++ src/tinylayeraroundgit/wizard/MergeBranchWizard.java
@@ -0,0 +1,55 @@
+package tinylayeraroundgit.wizard;
+
+import java.util.List;
+
+import org.eclipse.core.resources.IResource;
+import org.eclipse.jface.dialogs.MessageDialog;
+
+import temp.TempDebug;
+import tinylayeraroundgit.git.GitCommand;
+import tinylayeraroundgit.wizard.page.SelectBranchPage;
+
+
+public class MergeBranchWizard extends AppWizard {
+	
+	private SelectBranchPage selectBranchPage;
+
+	public MergeBranchWizard( List<IResource> selectedResources ) {
+		super( selectedResources );
+	}
+	
+	public void addPages() {
+		this.selectBranchPage = new SelectBranchPage( 
+				"Select branch", 
+				"Select branch", 
+				"Please select (or enter) to merge with the current one", 
+				this );
+		addPage( selectBranchPage );
+	}
+
+	@Override
+	public boolean performFinish() {
+		String branchName = selectBranchPage.getSelectedBranch();
+		
+		if( branchName != null ) {
+			branchName = branchName.replaceAll( "\\s", "" );
+		}
+		
+		if( branchName == null || branchName.trim().length() == 0 ) {
+			MessageDialog.openInformation( getShell(), "Invalid branch", "Invalid branch:" + branchName );
+		}
+		
+		GitCommand gitCommand = new GitCommand( "merge " + branchName, true );
+		
+		try {
+			gitCommand.executeOn( getSelectedResources() );
+			
+			return true;
+		} catch ( InterruptedException e ) {
+			MessageDialog.openInformation( getShell(), "Merge error", "Error merging with:" + branchName );
+			
+			return false;
+		}
+	}
+
+}
